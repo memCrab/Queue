488a15566ce84dd4caa555819165fa765547874c
Oleksandr Diudiun <a.diudiun@gmail.com>
2022-07-04 15:46:26 +0200

    Update QueueInterface.php

--- src/QueueInterface.php
+++ src/QueueInterface.php
@@ -7,12 +7,12 @@ namespace Memcrab\Queue;
 interface QueueInterface
 {
     public static function obj(): self;
-    public static function setConnectionProperties(array $properties): void;
-    public function connect(): self;
+    public static function declareConnection(array $properties, \Memcrab\Log\Log $ErrorHandler): void;
+    public function connect(): bool;
     public function registerQueue(string $name);
     public function sendMessage(string $name, array $messageBody);
     public function receiveMessage(string $name);
     public function client(): object;
-    public function connectionStatus(): bool;
+    public function ping(): bool;
     public static function shutdown(): void;
 }
